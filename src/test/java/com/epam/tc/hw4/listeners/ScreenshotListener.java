e79a1c0d2790772310f1f44af1f06af4327672e0
evgenii-podruzhinskii <evg.pod.dm@gmail.com>
2022-03-04 21:07:18 +0300

    Change allure versions

--- src/test/java/com/epam/tc/hw4/listeners/ScreenshotListener.java
+++ src/test/java/com/epam/tc/hw4/listeners/ScreenshotListener.java
@@ -1,6 +1,7 @@
 package com.epam.tc.hw4.listeners;
 
-import com.epam.tc.hw4.utils.AttachmentUtils;
+import io.qameta.allure.Allure;
+import java.io.ByteArrayInputStream;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
@@ -12,7 +13,7 @@ public class ScreenshotListener implements ITestListener {
         Object driver = result.getTestContext().getAttribute("driver");
         if (driver != null) {
             byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            AttachmentUtils.attachPngImage("Image on failure", screenshot);
+            Allure.addAttachment("Fail image", new ByteArrayInputStream(screenshot));
         } else {
             throw new RuntimeException("No driver was found");
         }
